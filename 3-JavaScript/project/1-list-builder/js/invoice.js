faab209f2b1c8fd85ba318a5add3878d1be1b694
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-07-30 14:51:48 +0630

    Invoice App

--- 3-JavaScript/project/1-list-builder/js/invoice.js
+++ 3-JavaScript/project/1-list-builder/js/invoice.js
@@ -13,30 +13,43 @@ const products = [
   },
   {
     id: 3,
-    name: "Mango",
-    price: 900,
-  },
-  {
-    id: 4,
     name: "Lime",
     price: 50,
   },
   {
-    id: 5,
-    name: "Lemon",
-    price: 500,
+    id: 4,
+    name: "Banana",
+    price: 200,
   },
 ];
+
+let rowCount = 1;
 
 const app = document.querySelector("#app");
 const newRecord = document.querySelector("#newRecord");
 const product = document.querySelector("#product");
 const quantity = document.querySelector("#quantity");
 const records = document.querySelector("#records");
+const inventories = document.querySelector("#inventories");
 const recordRows = document.querySelector("#record-rows");
 const recordsTotal = document.querySelector(".records-total");
+const newItem = document.querySelector("#newItem");
+const newItemName = document.querySelector("#newItemName");
+const newItemPrice = document.querySelector("#newItemPrice");
 
 // functions
+
+const createItem = (name, price) => {
+  const div = document.createElement("div");
+  div.className =
+    "item-list border border-2 p-3 mb-3 d-flex justify-content-between";
+  div.innerHTML = `
+  <p class=" mb-0 item-name">${name}</p>
+  <p class="text-black-50 mb-0 item-price">${price} mmk</p>
+  `;
+  return div;
+};
+
 const createRecordRow = (productId, quantity) => {
   // information
   const currentProduct = products.find((el) => el.id == productId);
@@ -44,18 +57,45 @@ const createRecordRow = (productId, quantity) => {
 
   const tableRow = document.createElement("tr");
   tableRow.classList.add("record-row");
+  tableRow.setAttribute("product-id", productId);
   tableRow.innerHTML = `
-          <td>1</td>
+          <td class='record-no'></td>
           <td class="text-start record-product">${currentProduct.name}</td>
           <td class="text-end record-price">${currentProduct.price}</td>
-          <td class="text-end record-quantity">${quantity.valueAsNumber}</td>
-          <td class="text-end record-cost">${cost}</td>
+          <td class="text-end ">
+          <span>
+            <i class=" record-quantity-control record-quantity-decrement bi bi-dash"></i>
+          </span>
+          <span class='record-quantity'>
+          ${quantity.valueAsNumber}
+          </span>
+          <span>
+            <i class=" record-quantity-control record-quantity-increment bi bi-plus"></i>
+          </span>
+          </td>
+          <td class="text-end position-relative">
+          <span class='record-cost'>${cost}</span>
+          <button class=" d-print-none btn btn-sm btn-primary record-row-del position-absolute">
+            <i class=" bi bi-trash3"></i>
+          </button>
+          </td>
       `;
+
+  tableRow.querySelector(".record-row-del").addEventListener("click", () => {
+    if (confirm("Are U sure to remove product ?")) {
+      tableRow.classList.add("animate__animated", "animate__fadeOut");
+      tableRow.addEventListener("animationend", () => {
+        tableRow.remove();
+        calculateTotal();
+      });
+    }
+  });
+
   return tableRow;
 };
 
 const calculateTotal = () => {
-  console.clear();
+  // console.clear();
 
   // let total = 0;
 
@@ -82,14 +122,32 @@ products.forEach((el) => {
   // newOption.innerText = el.name
   // newOption.value = el.id
   product.append(new Option(el.name, el.id));
+  inventories.append(createItem(el.name, el.price));
 });
 
 // add record
 newRecord.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  // create new row
-  recordRows.append(createRecordRow(product.value, quantity));
+  const isExistedRow = document.querySelector(
+    `[product-id='${product.value}']`
+  );
+
+  if (isExistedRow) {
+    let currentPrice = isExistedRow.querySelector(".record-price");
+    let currentQuantity = isExistedRow.querySelector(".record-quantity");
+    let currentCost = isExistedRow.querySelector(".record-cost");
+
+    let newQuantity =
+      parseFloat(currentQuantity.innerText) + quantity.valueAsNumber;
+    let newCost = currentPrice.innerText * newQuantity;
+
+    currentQuantity.innerText = newQuantity;
+    currentCost.innerText = newCost;
+  } else {
+    // create new row
+    recordRows.append(createRecordRow(product.value, quantity));
+  }
 
   // clear form
   newRecord.reset();
@@ -105,4 +163,25 @@ newRecord.addEventListener("submit", (e) => {
   // const formData = new FormData(newRecord);
   // console.log(formData.get("product"));
   // console.log(formData.get("quantity"));
+});
+
+newItem.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // products array update
+  let newItemId = products[products.length - 1].id + 1;
+  const newItemObj = {
+    id: newItemId,
+    name: newItemName.value,
+    price: newItemPrice.valueAsNumber,
+  };
+
+  products.push(newItemObj);
+
+  // form reset
+  newItem.reset();
+
+  // ui update
+  product.append(new Option(newItemObj.name, newItemObj.id));
+  inventories.append(createItem(newItemObj.name, newItemObj.price));
 });
